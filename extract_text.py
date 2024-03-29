e4a91f3a388911f35d7f8e3636d04636e42cba52
sarcoding <sarthak.coding@gmail.com>
2024-03-29 14:06:04 +0530

    Update

--- extract_text.py
+++ extract_text.py
@@ -19,12 +19,12 @@ def extract_amount(img, lines):
     pattern_amt = rf".*({amt_keys_pattern})\s+([$€])?\s*((\d{{1,3}}(?:,\d{{3}})*)(\.\d{{2}})?)"
     
     for i in range(len(lines)-1, 0, -1):
-        match_amt = re.match(pattern_amt, lines[i][1], re.IGNORECASE)
+        match_amt = re.match(pattern_amt, lines[i][5], re.IGNORECASE)
         if match_amt:
-            amount_text = lines[i][1]
+            # amount_text = lines[i][1]
             currency = match_amt.group(2) if match_amt.group(2) in valid_currencies else '₹'
             amount = float(match_amt.group(3).replace(',', ''))
-            x1, y1, x2, y2 = lines[i][0]
+            x1, y1, x2, y2 = lines[i][1:5]
             cv2.putText(img, "Amount", (x1, y2-25), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
@@ -38,10 +38,10 @@ def extract_gst_num(img, lines):
     gst_numbers = []
 
     for line in lines:
-        match_gst_num = re.match(pattern_gst_num, line[1], re.IGNORECASE)
+        match_gst_num = re.match(pattern_gst_num, line[5], re.IGNORECASE)
         if match_gst_num:
             gst_numbers.append(match_gst_num.group(3))
-            x1, y1, x2, y2 = line[0]
+            x1, y1, x2, y2 = line[1: 5]
             cv2.putText(img, "GSTIN", (x1, y2-25), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     return gst_numbers[:2]
@@ -50,22 +50,18 @@ def extract_to(img, blocks):
     to_keys_pattern = "|".join(re.escape(key) for key in keywords["to"])
     pattern_to = rf"\b({to_keys_pattern})\b"
     for i in range(len(blocks)):
-        match_to = re.match(pattern_to, blocks[i][6], re.IGNORECASE)
+        match_to = re.match(pattern_to, blocks[i][5], re.IGNORECASE)
         if match_to:
-            left, top, right, bottom = blocks[i+1][2:6]
+            left, top, right, bottom = blocks[i+1][1:5]
             cv2.putText(img, "To", (left, top-5), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
             cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
-            return blocks[i+1][6]
+            # print("____"*20)
+            # print(i, len(blocks))
+            return blocks[i+1][5]
     
     return "Not Found"
 
-def extract_info(img=None, example_image=None):
-    if example_image is not None:
-        img = example_image
-    
-    if img is None:
-        return {'error': 'No image provided'}
-    
+def extract_info(img=None):
     img = np.array(img)
     lines, blocks = parse_line_blocks(img=img)
     
@@ -76,6 +72,6 @@ def extract_info(img=None, example_image=None):
     return img, {'to': to_text, 'amt_currency': amount_currency, 'amt_value': amount_value, 'gst_numbers': gst_numbers}
 
 if __name__ == "__main__":
-    info, _ = extract_info(cv2.imread("Example1.jpg"))
-
-    print(f"TO: {info['to']}, Amount Currency: {info['amt_currency']}, Amount Value: {info['amt_value']}, GSTINs: {info['gst_numbers']}")
+    _, info = extract_info(img = cv2.imread("D:\Project\InvoiceScrapping\Final\Example1.jpg"))
+    print(info)
+    # print(f"TO: {info['to']}, Amount Currency: {info['amt_currency']}, Amount Value: {info['amt_value']}, GSTINs: {info['gst_numbers']}")
--- image_segmentation.py
+++ image_segmentation.py
@@ -11,16 +11,12 @@ def parse_line_blocks(img):
 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     boxes = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-    blocks = parse_blocks(img, boxes)
-    lines = parse_lines(img, boxes)
-    return lines, blocks
-
-    
-def parse_blocks(img, boxes):
     blocks = []
+    lines = []
     for i in range(len(boxes['text'])):
         if int(boxes['conf'][i]) > 50:
             block_num = boxes['block_num'][i]
+            line_num = boxes['line_num'][i]
             left = boxes['left'][i]
             top = boxes['top'][i]
             width = boxes['width'][i]
@@ -29,60 +25,122 @@ def parse_blocks(img, boxes):
 
             if text == " " or text == '':
                 continue
+            if len(lines) == 0:
+                lines.append([line_num, left, top, left + width, top + height, text])
+                blocks.append([block_num, left, top, left + width, top + height, text])
+                continue
+            if blocks[-1][0] == block_num:
+                if lines[-1][0] == line_num:
+                    lines[-1][1] = min(lines[-1][1], left)
+                    lines[-1][2] = min(lines[-1][2], top)
+                    lines[-1][3] = max(lines[-1][3], left + width)
+                    lines[-1][4] = max(lines[-1][4], top + height)
+                    lines[-1][5] += " " + text
+                else:
+                    lines.append([line_num, left, top, left + width, top + height, text])
+                blocks[-1][1] = min(blocks[-1][1], left)
+                blocks[-1][2] = min(blocks[-1][2], top)
+                blocks[-1][3] = max(blocks[-1][3], left + width)
+                blocks[-1][4] = max(blocks[-1][4], top + height)
+                blocks[-1][5] += " " + text
 
-            block_exists = False
-            for block in blocks:
-                if block[0] == block_num:
-                    block_exists = True
-                    block[2] = min(block[2], left)
-                    block[3] = min(block[3], top)
-                    block[4] = max(block[4], left + width)
-                    block[5] = max(block[5], top + height)
-                    block[6] = block[6] + " " + text if len(block) > 6 else text
-                    break
+            else:
+                lines.append([line_num, left, top, left + width, top + height, text])
+                blocks.append([block_num, left, top, left + width, top + height, text])
+    return lines, blocks
 
-            if not block_exists:
-                blocks.append([block_num, text, left, top, left + width, top + height, text])
-    return blocks
-
-def parse_lines(img, boxes):
+def check(lines):
+    for i, line in enumerate(lines):
+        if len(line) != 6:
+            print(i)
+    print(lines)
+    print("No error")
+def parse_blocks(boxes):
+    blocks = []
     lines = []
-    current_line = ''
-    line_start_coordinates = None
-    line_num_prev = None
+    for i in range(len(boxes['text'])):
+        if int(boxes['conf'][i]) > 50:
+            block_num = boxes['block_num'][i]
+            line_num = boxes['line_num'][i]
+            left = boxes['left'][i]
+            top = boxes['top'][i]
+            width = boxes['width'][i]
+            height = boxes['height'][i]
+            text = boxes['text'][i]
+
+            if text == " " or text == '':
+                continue
+            if len(lines) == 0:
+                lines.append([line_num, left, top, left + width, top + height, text])
+                blocks.append([block_num, left, top, left + width, top + height, text])
+                continue
+            if blocks[-1][0] == block_num:
+                if lines[-1][0] == line_num:
+                    lines[-1][1] = min(lines[-1][1], left)
+                    lines[-1][2] = min(lines[-1][2], top)
+                    lines[-1][3] = max(lines[-1][3], left + width)
+                    lines[-1][4] = max(lines[-1][4], top + height)
+                    lines[-1][5] += " " + text
+                else:
+                    lines.append([line_num, left, top, left + width, top + height, text])
+                blocks[-1][1] = min(blocks[-1][1], left)
+                blocks[-1][2] = min(blocks[-1][2], top)
+                blocks[-1][3] = max(blocks[-1][3], left + width)
+                blocks[-1][4] = max(blocks[-1][4], top + height)
+                blocks[-1][5] += " " + text
+
+            else:
+                lines.append([line_num, left, top, left + width, top + height, text])
+                blocks.append([block_num, left, top, left + width, top + height, text])
+
+def parse_lines(boxes):
+    lines = []
+    # current_line = ''
+    # line_start_coordinates = None
+    # line_num_prev = None
 
     for i in range(len(boxes['text'])):
-        text = boxes['text'][i].strip()
-        line_num = boxes['line_num'][i]
-        x, y, w, h = boxes['left'][i], boxes['top'][i], boxes['width'][i], boxes['height'][i]
-        line_coordinates = (x, y, x + w, y + h)
+        if int(boxes['conf'][i]) > 50:
+            line_num = boxes['line_num'][i]
+            line_left = boxes['left'][i]
+            line_top = boxes['top'][i]
+            line_width = boxes['width'][i]
+            line_height = boxes['height'][i]
+            line_text = boxes['text'][i]
 
-        if line_num != line_num_prev and line_num_prev is not None:
-            lines.append((line_start_coordinates, current_line.strip()))
-            current_line = ''
-            line_start_coordinates = None
+            if line_text == "" or line_text ==" ":
+                continue
 
-        if text:
-            if line_start_coordinates is None:
-                line_start_coordinates = line_coordinates
-            current_line += text + ' '
-
-        line_num_prev = line_num
-
-    if current_line.strip():
-        lines.append((line_start_coordinates, current_line.strip()))
-
-    filtered_lines = [(coords, text) for coords, text in lines if coords is not None and text.strip()]
-    
-    return filtered_lines
+            line_exist = False
+            for line in lines:
+                if line[0] == line_num:
+                    line_exist = True
+                    line[1] = min(line[1], line_left)
+                    line[2] = min(line[2], line_top)
+                    line[3] = max(line[3], line_left + line_width)
+                    line[4] = max(line[4], line_top + line_height)
+                    line[5] += " " + line_text
+                    break
+            if not line_exist:
+                lines.append([line_num, line_left, line_top, line_left + line_width, line_top + line_height, line_text])
+    img = cv2.imread("D:\Project\InvoiceScrapping\Final\Example1.jpg")
+    for line in lines:
+        x1, y1, x2, y2 = line[1:5]
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    print(lines)
+    cv2.imwrite("sample.jpg", img)
+        
+    return lines
 
 
 if __name__ == "__main__":
     img_path = 'sample2.png'
-    blocks = parse_blocks(cv2.imread(img_path))
-    lines = parse_lines(cv2.imread(img_path))
-    for line_info in lines:
-        line_coordinates, line_text = line_info
-        x1, y1, x2, y2 = line_coordinates
-        print(f"Line Text: {line_text}")
-        print(f"Coordinates: ({x1}, {y1}), ({x2}, {y2})")
+    # blocks = parse_blocks(cv2.imread(img_path))
+    lines, blocks = parse_line_blocks(cv2.imread("D:\Project\InvoiceScrapping\Final\Example1.jpg"))
+    check(blocks)
+    # lines = parse_blocks(boxes)
+    # for line_info in lines:
+    #     line_coordinates, line_text = line_info
+    #     x1, y1, x2, y2 = line_coordinates
+    #     print(f"Line Text: {line_text}")
+    #     print(f"Coordinates: ({x1}, {y1}), ({x2}, {y2})")
